da897879e9439db658a372f0a5e8ab88da80bf92
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2010-07-02 21:51:56 +0000

    Import of AVAR/Dist-Zilla-Plugin-MakeMaker-Awesome-0.09 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-MakeMaker-Awesome
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         AVAR/Dist-Zilla-Plugin-MakeMaker-Awesome-0.09.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Dist::Zilla::Plugin::MakeMaker::Awesome::AUTHORITY = 'cpan:AVAR';
 }
 BEGIN {
-  $Dist::Zilla::Plugin::MakeMaker::Awesome::VERSION = '0.08';
+  $Dist::Zilla::Plugin::MakeMaker::Awesome::VERSION = '0.09';
 }
 
 use Moose;
@@ -407,6 +407,20 @@ C<package MY> section to install it. Deep magic.
 The main entry point is C<setup_installer> via the
 L<Dist::Zilla::Role::InstallTool> role. There are also other magic
 Dist::Zilla roles, check the source for more info.
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item attempt to add Makefile.PL multiple times
+
+This error from L<Dist::Zilla> means that you've used both
+C<[MakeMaker]> and C<[MakeMaker::Awesome]>. You've either included
+C<MakeMaker> directly in F<dist.ini>, or you have plugin bundle that
+includes it. See L<@Filter|Dist::Zilla::PluginBundle::Filter> for how
+to filter it out.
+
+=back
 
 =head1 BUGS
 
